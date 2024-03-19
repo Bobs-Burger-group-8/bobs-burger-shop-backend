1450fadf3e0b3de063197cf56344d56fb66558b2
kristianverduin <kristianverduin@gmail.com>
2024-03-19 15:09:10 +0100

    Refactor

--- bobs-burger-api/Endpoints/AuthEndpoints.cs
+++ bobs-burger-api/Endpoints/AuthEndpoints.cs
@@ -19,52 +19,60 @@ namespace bobs_burger_api.Endpoints
             taskGroup.MapPost("/register", Register);
             taskGroup.MapPost("/login", Login);
         }
-        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async static Task<IResult> Register(RegisterDto registerPayload, UserManager<ApplicationUser> userManager)
         {
             if (registerPayload.Email == null) 
                 return TypedResults.BadRequest("Email is required.");
             if (registerPayload.Password == null) 
                 return TypedResults.BadRequest("Password is required.");
+
             var result = await userManager.CreateAsync(
-            new ApplicationUser
-            {
-                UserName = registerPayload.Email,
-                Email = registerPayload.Email,
-                Role = UserRole.User
-            },
-            registerPayload.Password!
-            );
+                new ApplicationUser
+                {
+                    UserName = registerPayload.Email,
+                    Email = registerPayload.Email,
+                    Role = UserRole.User
+                },
+                registerPayload.Password!);
+
             if (result.Succeeded)
             {
-                return TypedResults.Created($"/auth/", new
-                RegisterResponseDto(registerPayload.Email, UserRole.User));
+                return TypedResults.Created($"/auth/", new RegisterResponseDto(registerPayload.Email, UserRole.User));
             }
+
             return Results.BadRequest(result.Errors);
         }
 
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async static Task<IResult> Login(LoginDto loginPayload, UserManager<ApplicationUser> userManager, TokenService tokenService)
         {
             if (loginPayload.Email == null) 
                 return TypedResults.BadRequest("Email is required.");
             if (loginPayload.Password == null) 
                 return TypedResults.BadRequest("Password is required.");
+
             var user = await
             userManager.FindByEmailAsync(loginPayload.Email!);
+
             if (user == null)
             {
                 return TypedResults.BadRequest("Invalid email or password.");
             }
-            // check the password matches
+            
             var isPasswordValid = await
             userManager.CheckPasswordAsync(user, loginPayload.Password);
+
             if (!isPasswordValid)
             {
                 return TypedResults.BadRequest("Invalid email or password.");
             }
-            // create a token
+            
             var token = tokenService.CreateToken(user);
-            // return the response
+            
             return TypedResults.Ok(new AuthResponseDto(token, user.Email, user.Role));
         }
     }
